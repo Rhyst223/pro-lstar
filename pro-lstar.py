e9ef47055bb761cc16a474f69dd0752af4a62519
Rhyst223 <31656440+Rhyst223@users.noreply.github.com>
2020-10-21 14:17:13 +0100

    Update pro-lstar.py
    Fixed some bugs. Ready for first release (custom data retrieval)

--- pro-lstar.py
+++ pro-lstar.py
@@ -7,11 +7,17 @@ import numpy as np
 from itertools import groupby, count
 from scipy.interpolate import griddata
 import warnings
-#warnings.filterwarnings("ignore")
+from copy import deepcopy
 
 grid_lats = np.array([50.77, 54.74, 57.69, 60.0, 61.87, 63.43, 64.76, 65.91, 66.91, 67.79, 68.58, 69.3, \
              69.94, 70.53, 71.07, 71.57])
 grid_lons = np.arange(0,360,15)
+
+class MissingDataWarning(UserWarning):
+    pass
+
+class InterpolationWarning(UserWarning):
+    pass
 
 def find_nearest(arr,m):
     '''
@@ -25,7 +31,7 @@ def find_nearest(arr,m):
 
     return [below,above]
 
-class CustomData():
+class get_period():
     """
     Class managing raw data for a given period/location/MF model. Finds and reads requested days
     from L* directory. Period must be continuous. As current L* files are yearly and take time to
@@ -33,9 +39,7 @@ class CustomData():
     file is already in memory to speed up process.
     ### Input parameters:
     _start_: (string) Start of period. Resolution must be YYYYMMDDHH  
-    _end_: (int) End of period. Assumed to inclusively close period (at midnight with no hourly resolution given)  
-    _mlat_: (float) Magnetic latitude   
-    _mlon_: (float) Magnetic longitude, in range (0-330 degrees)   
+    _end_: (int) End of period. Assumed to inclusively close period (at midnight with no hourly resolution given)   
     _MFmodel_: (list) Magnetic field model to retrieve data for. Assumed all of them. If not one of\
                 ['IGRF','OPQUIET','OSTA','T89','T96','T01QUIET','T01STORM','T05']
     _variable_: Variable to read in. Assumed only L*. Possible are [l,lm,b,x,y,z]. Denote l by blank string
@@ -43,12 +47,10 @@ class CustomData():
                         Default is None (provide model outputs only). If given must be <= len(MFmodel)
     """
     
-    def __init__(self, start, end, mlat, mlon, MFmodel = 'all', variable = [''],model_threshold=None):
+    def __init__(self, start, end, MFmodel = 'all', variable = [''],model_threshold=None):
         
         self.start = start #string
         self.end = end #string
-        self.mlat = mlat #float
-        self.mlon = mlon % 360 #float
         self.variable = variable
         self.MFmodel = MFmodel #list
         self.model_threshold = model_threshold
@@ -56,10 +58,6 @@ class CustomData():
         # check that given period is within data period
         assert ((start.year >2005) and (end.year <2017)),\
         "Period must be within the yearly range 2006-2016"
-        
-        # check that given mlat is within range
-        assert ((mlat >=50.77) and (mlat <=71.57)),\
-        "Magnetic latitude must be between 50.77 and 71.57"
         
         if ((model_threshold is not None) and (MFmodel !='all')):
             assert (model_threshold <= len(MFmodel)),\
@@ -69,7 +67,12 @@ class CustomData():
         if MFmodel is not 'all':
             assert all(m in ['IGRF','OPQUIET','OSTA','T89','T96','T01QUIET','T01STORM','T05'] for m in MFmodel),\
             "MFmodels must be in ['IGRF','OPQUIET','OSTA','T89','T96','T01QUIET','T01STORM','T05'] or all"
-
+        
+        #Check if given period contains non data days and provide warning if it does:
+        if any(pd.to_datetime(d,infer_datetime_format=True) in pd.date_range(start,end) \
+               for d in ['07/03/2006','19/02/2012','28/06/2013','29/06/2013','20/12/2015']):
+            warnings.warn('Given period contains dates with missing data. Be wary of x-axis'\
+                          +' in future plot_location_data() figures',MissingDataWarning)
         # now populate with data
         self.getData()
         
@@ -80,7 +83,7 @@ class CustomData():
         a pandas dataframe.
         """
         
-        def read_yearly_data(f,sdate,edate,mlon,mlat,cols):
+        def read_yearly_data(f,sdate,edate,cols):
             '''
             Function to read yearly L* files from data archive, and slice between start and
             end period
@@ -90,59 +93,8 @@ class CustomData():
             df = pd.read_csv(io.BytesIO(data),index_col=0,usecols=cols)  
             df = df.loc[str(sdate):str(edate)]
             df.mlat = df.mlat.apply(lambda x: round(x,2))
-            #df = df[(df.mlat == mlat) & (df.mlon == mlon)]
             
-            interp=''
-            if (mlon not in grid_lons): 
-                print('Off grid longitude - Interpolation required')
-                mlon_slice = find_nearest(grid_lons,mlon)
-                interp+='lon'
-            else:
-                mlon_slice = [mlon]
-                
-            if (round(mlat,2) not in grid_lats): 
-                print('Off grid latitude - Interpolation required')
-                mlat_slice = find_nearest(grid_lats,mlat)
-                interp+='lat'
-            else:
-                mlat_slice = [mlat]
-    
-            df = df[(df.mlat.isin(mlat_slice)) & (df.mlon.isin(mlon_slice))] 
-        
-            if interp !='':
-                interp_df = pd.concat([tdf.apply(interpolate_custom_data,axis=0,\
-                                                 args=(np.array([[a,b] for a,b in zip(tdf.mlon,tdf.mlat)]),66,4)) \
-                                       for time,tdf in df.groupby('0')],axis=1).T
-                interp_df.index = df.index.unique()
-                #interp_df['mlat'] = [mlat]*len(interp_df)
-                #interp_df['mlon'] = [mlon]*len(interp_df)
-                return interp_df
-            else: 
-                return df
-        
-        def interpolate_custom_data(series,coords,mlat,mlon):
-            '''
-            Function to interpolate custom period variables if location is off Pro-L* grid
-            '''
-            if ((series.name=='mlt') and (series[:2].is_monotonic_increasing==False)):
-                series[1]+=24
-                series[-1]+=24
-                    
-            if pd.isnull(series).any()==True:
-                warnings.warn('Interpolation not possible for '+series.name+' at time '+str(series.index.unique()[0])+' due to undefined L*(s)'+\
-                      ' at neighbouring points in Pro-L* domain')
-                return np.nan
-            else:
-                xnew = np.array([coords[:,0][0],mlon,coords[:,0][1]])
-                if (xnew[-1] == 0):
-                    xnew[-1]+=360
-                ynew = np.array([min(coords[:,1]),mlat,max(coords[:,1])])
-                znew = griddata(coords, series, (xnew[None,:], ynew[:,None]), method='linear')
-                
-                if series.name == 'mlt':
-                    return znew[1][1] % 24
-                else:
-                    return znew[1][1]
+            return df
             
         urls = ['https://researchdata.reading.ac.uk/222/9/2006.csv.gz',
                 'https://researchdata.reading.ac.uk/222/11/2007.csv.gz',
@@ -165,17 +117,97 @@ class CustomData():
         
         if self.start.year != self.end.year:
             print('Period covers multiple years - expect longer read in time')
-            url = [u for u in urls if u[-11:-7] in [str(self.start.year),str(self.end.year)]]
+            url = [u for u in urls if u[-11:-7] in \
+                   list(pd.date_range(self.start.year,self.end.year).year.unique())]
             
-            #df = pd.concat([read_yearly_data(u,self.start,self.end,cols=cols) for u in url])
+            df = pd.concat([read_yearly_data(u,self.start,self.end,cols) for u in url])
         else:
             url = [u for u in urls if u[-11:-7]==str(self.start.year)]
             
-            df = read_yearly_data(url[0],self.start,self.end,self.mlon,self.mlat,cols=cols)
+            df = read_yearly_data(url[0],self.start,self.end,cols)
             
-        self.data = df
+        self.period_data = df
         
-    def plot_custom_data(self):
+    def get_location(self,mlat, mlon):
+        '''
+        Get location data for the downloaded period, interpolating if off-grid in Pro-L*
+        Inputs:
+        _mlat_: (float) Magnetic latitude   
+        _mlon_: (float) Magnetic longitude, in range (0-330 degrees)  
+        '''
+        
+        def interpolate_custom_data(series,coords,mlat,mlon):
+            '''
+            Function to interpolate custom period variables if location is off Pro-L* grid
+            '''
+            if ((series.name=='mlt') and (series[:2].is_monotonic_increasing==False)):
+                series[1]+=24
+                series[-1]+=24
+                    
+            if pd.isnull(series).any()==True:
+                #warnings.warn('Interpolation not possible for '+series.name+' at time '+str(series.index.unique()[0])+' due to undefined L*(s)'+\
+                 #     ' at neighbouring points in Pro-L* domain',InterpolationWarning)
+                return np.nan
+            else:
+                xnew = np.array([coords[:,0][0],mlon,coords[:,0][1]])
+                if (xnew[-1] == 0):
+                    xnew[-1]+=360
+                ynew = np.array([min(coords[:,1]),mlat,max(coords[:,1])])
+                znew = griddata(coords, series, (xnew[None,:], ynew[:,None]), method='linear')
+                
+                if series.name == 'mlt':
+                    return znew[1][1] % 24
+                else:
+                    return znew[1][1]
+                
+        mlon = mlon % 360
+        
+        # check that given mlat is within range
+        assert ((mlat >=50.77) and (mlat <=71.57)),\
+        "Magnetic latitude must be between 50.77 and 71.57"
+        
+        interp=''
+        if (mlon not in grid_lons): 
+            print('Off grid longitude - Interpolation required')
+            mlon_slice = find_nearest(grid_lons,mlon)
+            interp+='lon'
+        else:
+            mlon_slice = [mlon]
+
+        if (round(mlat,2) not in grid_lats): 
+            print('Off grid latitude - Interpolation required')
+            mlat_slice = find_nearest(grid_lats,mlat)
+            interp+='lat'
+        else:
+            mlat_slice = [mlat]
+
+        df = self.period_data[(self.period_data.mlat.isin(mlat_slice)) & \
+                              (self.period_data.mlon.isin(mlon_slice))] 
+
+        if interp !='':
+            interp_df = pd.concat([tdf.apply(interpolate_custom_data,axis=0,\
+                                             args=(np.array([[a,b] for a,b in zip(tdf.mlon,tdf.mlat)]),mlat,mlon)) \
+                                   for time,tdf in df.groupby('0')],axis=1).T
+            interp_df.index = df.index.unique()
+            #self.data = interp_df
+            locdf = deepcopy(self)
+            locdf.period_data = None
+            locdf.data = interp_df
+            locdf.mlat = mlat
+            locdf.mlon = mlon % 360
+            return locdf
+        else: 
+            
+            locdf = deepcopy(self)
+            locdf.period_data = None
+            locdf.data = df
+            locdf.mlat = mlat
+            locdf.mlon = mlon % 360
+            #self.data = df
+            return locdf
+        
+        
+    def plot_location_data(self):
         '''
         Function to plot variables over given period, including shading for MLT
         '''
@@ -200,7 +232,12 @@ class CustomData():
 
             return lval, hval
         
-        fig, ax = plt.subplots(len(self.variable),1,figsize=(10,5*len(self.variable)),sharex='all')
+        #Check that user has actually filtered period data for a certain location
+        assert hasattr(self, 'data'),\
+        "plot_location_data() requires filtered location data following get_period. Please"\
+            +" create location object with get_location() and apply this function to that"
+        
+        fig, ax = plt.subplots(len(self.variable),1,figsize=(12,8),sharex='all')
         
         styles = ['+','o','*','s','d','1','x','h'][:len(self.MFmodel)]
         plot_vars = [[m + '_' + v if v!='' else m for m in self.MFmodel] for v in self.variable]
@@ -236,11 +273,18 @@ class CustomData():
         [this_ax.set_xlabel('') if v!='' else this_ax.set_ylabel('L*') for \
          this_ax,v in zip(ax,self.variable)]
         
+        #fig.tight_layout()
         #ax[0].legend(bbox_to_anchor=(1, 1.1),ncol=len(self.MFmodel))
-        ax[0].legend(loc='upper center', bbox_to_anchor=(0.5, 1.1),
-          ncol=len(self.MFmodel), fancybox=True, shadow=True)
+        if self.model_threshold is not None:
+            ax[0].legend(loc='upper center', bbox_to_anchor=(0.5, 1.5),
+              ncol=len(self.MFmodel)+1, fancybox=True, shadow=True)
+        else:
+            ax[0].legend(loc='upper center', bbox_to_anchor=(0.5, 1.5),
+              ncol=len(self.MFmodel), fancybox=True, shadow=True)
         plt.xticks(rotation=90)
         plt.show()
+        
+ 
         
  
         
